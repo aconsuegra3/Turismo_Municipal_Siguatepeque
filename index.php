5ac1ebe00d6d1c405da3fcc1967c7adfa85a716d
Abel Consuegra <enriquezun3@gmail.com>
2020-10-19 11:29:45 -0600

    actualizando la página de inicio, agregando los assets para img

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-- cambiar el tipo hotel, agregar piscina, cambiar colores -->
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -22,21 +24,54 @@
 <body class="d-flex flex-column h-100 ">
 
     <nav class="navbar navbar-dark">
-        <a class="mr-auto ml-4" href="index.php"><i class="fas fa-home text-light" style="font-size: 25px;"></i></a>
-        <img class="mr-auto" src="img/logo_muni2.png" width="50px" alt="">
+        <!-- <a class="mr-auto ml-4" href="index.php"><i class="fas fa-sync-alt text-light" style="font-size: 25px;"></i></a> -->
+        <a class="mr-auto" href="index.php"><img src="img/logo_turismo.png" width="50px" alt=""></a>
+        <div class="mr-5 text-light">
+            <script type="text/javascript">
+                //<![CDATA[
+                var today = new Date();
+                var m = today.getMonth() + 1;
+                var mes = (m < 10) ? '0' + m : m;
+                document.write('Fecha: ' + today.getDate(), '/' + mes, '/' + today.getYear());
+                //]]> 
+            </script>
+        </div>
+        <script type="text/javascript">
+            function startTime() {
+                today = new Date();
+                h = today.getHours();
+                m = today.getMinutes();
+                s = today.getSeconds();
+                m = checkTime(m);
+                s = checkTime(s);
+                document.getElementById('reloj').innerHTML = h + ":" + m + ":" + s;
+                t = setTimeout('startTime()', 500);
+            }
+
+            function checkTime(i) {
+                if (i < 10) {
+                    i = "0" + i;
+                }
+                return i;
+            }
+            window.onload = function() {
+                startTime();
+            }
+        </script>
+        <div class="text-light mr-5" id="reloj" style="font-size:20px;"></div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
                 <li class="nav-item mx-auto ">
-                    <a class="nav-link" href="index.php"> <i class="fas fa-home mr-1"></i> Inicio</a>
+                    <hr class="my-0" width="400px"> <a class="nav-link text-center" href="index.php"> <i class="fas fa-home mr-1"></i> Inicio</a> <hr class="my-0"> 
                 </li>
                 <li class="nav-item mx-auto ">
-                    <a class="nav-link" href="alojamiento/alojamiento.php"><i class="fas fa-hotel mr-1"></i> Alojamiento</a>
+                    <a class="nav-link text-center" href="alojamiento/alojamiento.php"><i class="fas fa-hotel mr-1"></i> Alojamiento</a><hr class="my-0" width="400px">
                 </li>
                 <li class="nav-item mx-auto">
-                    <a class="nav-link" href="alimentos_y_bebidas/alimentosYBebidas.php"><i class="fas fa-utensils mr-1"></i> Alimentos y bebidas</a>
+                    <a class="nav-link text-center" href="alimentos_y_bebidas/alimentosYBebidas.php"><i class="fas fa-utensils mr-1"></i> Alimentos y bebidas</a><hr class="my-0" width="400px">
                 </li>
             </ul>
         </div>
@@ -46,12 +81,12 @@
         <!-- <h1 class="text-center mt-5 mb-5">Turismo Municipal Siguatepeque</h1> -->
         <img class=" mb-4" src="img/banner_muni.jpg" alt="Turismo Municipal Siguatepeque">
         <div class="container mt-4 menuInicio" style="display: flex; flex-direction: column; align-items:center;">
-            <a class="btn btn-primary my-3 " href="alojamiento/alojamiento.php">SERVICIOS DE ALOJAMIENTO</a>
-            <a class="btn btn-primary my-3 mt-4" href="alimentos_y_bebidas/alimentosYBebidas.php">ALIMENTOS Y BEBIDAS</a>
+            <a class="btn btn-success my-3 " href="alojamiento/alojamiento.php">SERVICIOS DE ALOJAMIENTO</a>
+            <a class="btn btn-success my-3 mt-4" href="alimentos_y_bebidas/alimentosYBebidas.php">ALIMENTOS Y BEBIDAS</a>
         </div>
     </div>
 
-    <footer class="footer mt-5 py-3 text-center">
+    <footer class="footer mt-3 py-3 text-center">
         <div class="container">
             <hr>
             <span class="text-muted">© Abel Consuegra - 2020</span><br><br>
